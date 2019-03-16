60335c111323e247d593be5e0123825cbbaed372
Matt Doughty <m.doughty@hushmail.com>
2019-03-16 21:16:04 +0000

    Adding turn tests.

---
+++ test/mars_adventure/robot_test.exs
@@ -0,0 +1,30 @@
+defmodule MarsAdventure.RobotTest do
+  use ExUnit.Case
+
+  alias MarsAdventure.Location
+  alias MarsAdventure.Robot
+
+  describe "navigating the robot" do
+    test "the robot can turn left" do
+      {:ok, robot} = Robot.new(Location.new(0, 0), "E")
+
+      robot_after_one_turn = Robot.turn_left(robot)
+      robot_after_two_turns = Robot.turn_left(robot_after_one_turn)
+      robot_after_three_turns = Robot.turn_left(robot_after_two_turns)
+      robot_after_four_turns = Robot.turn_left(robot_after_three_turns)
+
+      assert robot_after_one_turn.orientation == "N"
+      assert robot_after_two_turns.orientation == "W"
+      assert robot_after_three_turns.orientation == "S"
+      assert robot_after_four_turns.orientation == "E"
+    end
+
+    test "the robot can turn right" do
+
+    end
+
+    test "the robot can move forward" do
+
+    end
+  end
+end
